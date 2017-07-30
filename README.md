6cb9207d72c7b65436c526ac9fc39ef01c2e74a8
Vital Krasilnikau <vital.krasilnikau@gmail.com>
2017-07-31 00:40:10 +0300

    Updated according to latest comment at stackoverflow

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Incremental backup Git repo to a git bundles, including all branches.
 
 
 ## Alpha version yet. But seems it works (there are tests).
+
+Updated according to latest [comment](https://stackoverflow.com/questions/12129148/incremental-backups-with-git-bundle-for-all-branches#comment-21364902).
--- git-inc-backup.pl
+++ git-inc-backup.pl
@@ -5,6 +5,7 @@ use warnings;
 
 use List::Util qw/first/;
 use Cwd;
+use File::Copy qw(copy);
 
 sub in_dir($&)
 {
@@ -30,29 +31,26 @@ my $remote_name = 'backup_bundle';
 my $git_command = qq{git --git-dir $gitdir};
 
 if ($command eq 'backup') {
-
+	my $remote_bundle_file_name = sprintf("%s%s.bundle", $backupdir, $name);
 	my @existing = get_existing_backups();
 	if (@existing) {
 		$existing[-1] =~ /\_(\d+).bundle$/;
 		my ($old_filename, $new_filename) = map { sprintf("%s%s_%04d.bundle", $backupdir, $name, $_) } ($1, $1 + 1);
-		cmd(qq{$git_command remote add $remote_name $old_filename});
-		
+
 		cmd(qq{$git_command fetch $remote_name});
 		my $refs = join(" ", get_local_branches(), map { "^$_" } get_remote_branches());
-		
+
 		cmd(qq{$git_command bundle create $new_filename $refs});
-		
-		cmd($git_command, qq{bundle}, qq{create},
-			sprintf("%s%s_%04d.bundle", $backupdir, $name, map { $1 + 1 } (sort glob($backupdir.$name."_*.bundle"))[-1] =~ /\_(\d+).bundle$/),
-			map { /^[\s\*]*(.*)$/; $1 } cmd($git_command, qq{branch}),
-			map { "^$_" } map { /^\s*(.*)$/; $1 } cmd($git_command, qq{branch -r}));
-		cmd(qq{$git_command remote rm $remote_name});
+		copy $new_filename, $remote_bundle_file_name;
 	} else {
 		print "FIRST\n";
 		my $new_filename = sprintf("%s%s_%04d.bundle", $backupdir, $name, 1);
+
 		cmd(qq{$git_command bundle create $new_filename --all});
+		copy $new_filename, $remote_bundle_file_name;
+		cmd(qq{$git_command remote add $remote_name $remote_bundle_file_name});
 	}
-	
+
 } elsif ($command eq 'restore') {
 	my $i = 0;
 	for (get_existing_backups()) {
@@ -64,11 +62,11 @@ if ($command eq 'backup') {
 			cmd(qq{git --git-dir ${gitdir}.git remote add $remote_name $_});
 			cmd(qq{git --git-dir ${gitdir}.git fetch $remote_name});
 			print cmd(qq{git --git-dir ${gitdir}.git branch -r});
-			
+
 			in_dir $gitdir => sub {
 				for (grep { defined } map { m!^\s*\Q$remote_name\E/(.*)$! ? $1 : undef } cmd(qq{git --git-dir ${gitdir}.git branch -r})) {
 					# TODO: work-dir simply not work here
-					# http://stackoverflow.com/questions/11292057/git-windows-git-pull-cannot-be-used-without-a-working-tree 
+					# http://stackoverflow.com/questions/11292057/git-windows-git-pull-cannot-be-used-without-a-working-tree
 					cmd(qq{git --git-dir ${gitdir}.git checkout $_});
 					cmd(qq{git --git-dir ${gitdir}.git pull $remote_name $_});
 				}
